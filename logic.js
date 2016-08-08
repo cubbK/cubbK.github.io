0bd181825749d955b7039d0b3a5fdb0cd4308429
cubbic <danpopa1998@gmail.com>
2016-08-08 21:43:36 +0300

    pentru mobil dejactivam aproape toata anim

--- logic.js
+++ logic.js
@@ -1,38 +1,44 @@
 $( document ).ready(function () {
-  var num = 0;
+  if (/Mobi/.test(navigator.userAgent)) {
+    // mobile!
+    $('*').css('visibility' ,'visible');
+  }else {
+    var num = 0;
 
-  $('.block-content').on('inview', function(event, isInView) {
-    if (isInView) {
-      $(this).css('visibility' ,'visible');
-      var anim = careParte(num);
-      num++;
+    $('.block-content').on('inview', function(event, isInView) {
+      if (isInView) {
+        $(this).css('visibility' ,'visible');
+        var anim = careParte(num);
+        num++;
 
-      $(this).addClass( "animated " +anim );
-    } else {
-      $(this).css('visibility' ,'hidden');
-      $(this).removeClass('animated fadeInLeft fadeInRight');
+        $(this).addClass( "animated " +anim );
+      } else {
+        $(this).css('visibility' ,'hidden');
+        $(this).removeClass('animated fadeInLeft fadeInRight');
+      }
+    });
+    function careParte(num){
+      if(num%2 == 0){
+
+        return 'fadeInLeft';
+      }else{
+
+        return 'fadeInRight';
+      }
     }
-  });
-  function careParte(num){
-    if(num%2 == 0){
 
-      return 'fadeInLeft';
-    }else{
+    $('.contactsAndEducation').on('inview', function(event, isInView) {
+      if (isInView) {
+        $(this).css('visibility' ,'visible');
+        $('.education').addClass('animated fadeInLeft');
+        $('.contact').addClass(' animated fadeInRight');
 
-      return 'fadeInRight';
-    }
+      }else{
+        $(this).css('visibility' ,'hidden');
+        $('.education').removeClass('animated fadeInLeft ');
+        $('.contact').removeClass('animated fadeInRight');
+      }
+    });
+
   }
-
-  $('.contactsAndEducation').on('inview', function(event, isInView) {
-    if (isInView) {
-      $(this).css('visibility' ,'visible');
-      $('.education').addClass('animated fadeInLeft');
-      $('.contact').addClass(' animated fadeInRight');
-
-    }else{
-      $(this).css('visibility' ,'hidden');
-      $('.education').removeClass('animated fadeInLeft ');
-      $('.contact').removeClass('animated fadeInRight');
-    }
-  });
-})
+  })
