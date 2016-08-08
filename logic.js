6beea43b83c5e0891aab0f96ea6dc7a169a3262c
cubbic <danpopa1998@gmail.com>
2016-08-08 14:36:39 +0300

    terminat cu adaugarea proiectelor

--- logic.js
+++ logic.js
@@ -2,9 +2,11 @@ $( document ).ready(function () {
   $('.block-content').on('inview', function(event, isInView) {
   if (isInView) {
     $(this).css('visibility' ,'visible');
+
     $(this).addClass( "animated bounceInRight" );
   } else {
-    
+    $(this).css('visibility' ,'hidden');
+    $(this).removeClass('animated bounceInRight');
   }
 });
 
