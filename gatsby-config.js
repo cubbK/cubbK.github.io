4c92b55c907412e582f2cb1fa7031eb17cd91f69
Dan <danpopa1998@gmail.com>
2017-10-29 19:49:41 +0200

    add sidebar

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,10 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Yo`,
   },
-  plugins: [`gatsby-plugin-react-helmet`],
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-stylus`
+  ],
+  
 }
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,62 +3,36 @@ import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
 import Helmet from 'react-helmet'
 
-import './index.css'
+import logo from './logo.png'
 
-const Header = () => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          Gatsby
-        </Link>
-      </h1>
+import './resets.css'
+import styles from './styles.module.styl'
+
+
+const Sidebar = () => (
+  <div className={styles.sidebar}>
+    <div className={styles.logo}>
+      <Link to="/">
+        <img src={logo} alt="Main Logo" />
+      </Link>
     </div>
   </div>
 )
 
 const TemplateWrapper = ({ children }) => (
-  <div>
+  <div className={styles.main}>
     <Helmet
-      title="Gatsby Default Starter"
+      title="Yo"
       meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
+        { name: 'description', content: 'My portfolio' },
+        { name: 'keywords', content: 'portfolio, cubbk, job' },
       ]}
     />
-    <Header />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
+    <Sidebar />
+    <div>
       {children()}
     </div>
   </div>
 )
-
-TemplateWrapper.propTypes = {
-  children: PropTypes.func,
-}
 
 export default TemplateWrapper
